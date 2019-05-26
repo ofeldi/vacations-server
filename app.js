23be72a75955aa3c32e0dd982f6d68bb82599fc2
Oren Feldman <Oren.Feldman@telit.com>
2019-05-26 21:12:17 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const vacationsRouter = require('./routes/vacations');
+
+var app = express();
+
+const notFoundPage = (path) => {
+    return (req, res, next) => {
+        res.sendFile(path);
+    }
+};
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/vacations', vacationsRouter);
+
+app.use(notFoundPage(path.join(__dirname, 'public', '404.html')));
+
+module.exports = app;
